ac0ec2c526824a6c2aedd05f5d2e605eb8519a66
ahsanreal4 <ahsan.btph123@gmail.com>
2024-10-17 16:35:42 +0500

    add Exception Messages file

---
+++ src/main/java/com/jobs/linkedIn/constants/ExceptionMessages.java
@@ -0,0 +1,29 @@
+package com.jobs.linkedIn.constants;
+
+public class ExceptionMessages {
+
+    /*
+    ==================================
+                USER
+    ==================================
+     */
+    public static String USER_NAME_ALREADY_EXISTS = "username already exists";
+    public static String EMAIL_ALREADY_EXISTS = "email already exists";
+    public static String USER_DOES_NOT_EXIST = "user does not exist";
+    public static String USER_ROLE_DOES_NOT_EXIST = "user role does not exist";
+    public static String USER_WTIH_THIS_EMAIL_DOES_NOT_EXIST = "User with this email doesn't exist";
+    public static String INVALID_CREDENTIALS = "Invalid Credentials";
+
+
+    /*
+    ==================================
+                POST
+    ==================================
+     */
+    public static String POST_DOES_NOT_EXIST = "post does not exist";
+
+
+    public static String REQUESTED_RESOURCE_NOT_EXIST = " does not exist";
+    public static String SOMETHING_WENT_WRONG = "Something went wrong in the server";
+
+}
